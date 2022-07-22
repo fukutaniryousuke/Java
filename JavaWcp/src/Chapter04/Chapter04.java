e4d9a8338ed3b8a8fe1a18800e75ea83230556a7
福谷亮介 <fukutaniryousuke@fukutaniryousukenoMacBook-Pro.local>
2022-07-22 20:43:43 +0900

    firstcommit

---
+++ JavaWcp/src/Chapter04/Chapter04.java
@@ -0,0 +1,13 @@
+package Chapter04;
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str;
+		str = "福谷";
+		System.out.println(str);
+		str = "一蓮托生";
+		System.out.println(str);
+		final int num = 7;
+		System.out.println(num);
+	}
+}
