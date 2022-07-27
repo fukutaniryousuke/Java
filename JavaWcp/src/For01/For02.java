04240ccc5c40e7ee9acb8df8a762302dd5173b2e
fukutaniryousuke <Fr83653554@gmail.com>
2022-07-27 14:48:58 +0900

    拡張For文

---
+++ JavaWcp/src/For01/For02.java
@@ -0,0 +1,10 @@
+package For01;
+
+public class For02 {
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5 };
+		for (int number : array) {
+			System.out.println(number);
+		}
+	}
+}
